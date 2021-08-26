31d3dd6aeb07201d243b2e6d1705662f6b5d022a
unknown <choihh0316@gmail.com>
2021-08-26 14:22:28 +0900

    init project

---
+++ src/main/java/miniproject/KUrestaurant/KUrestaurantApplication.java
@@ -0,0 +1,13 @@
+package miniproject.KUrestaurant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KUrestaurantApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KUrestaurantApplication.class, args);
+	}
+
+}
---
+++ src/test/java/miniproject/KUrestaurant/KUrestaurantApplicationTests.java
@@ -0,0 +1,13 @@
+package miniproject.KUrestaurant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KUrestaurantApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
